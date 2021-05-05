a61a5741aa4d4d03cf1eb9df7e4c1c11ca037329
Kostya <gazinaft@gmail.com>
2021-05-05 23:00:38 +0300

    final commit

---
+++ features/environment.py
@@ -0,0 +1,19 @@
+from behave import fixture, use_fixture
+from selenium import webdriver
+from pageObjects.brieflyPO import BrieflyPO
+from pageObjects.duckDuckGoPO import DuckDuckGoPO
+
+@fixture
+def browser_firefox(context):
+
+    browser = webdriver.Firefox()
+    context.duckduckgo = DuckDuckGoPO(browser)
+    context.briefly = BrieflyPO(browser)
+
+    yield context
+    
+    browser.quit()
+
+
+def before_all(context):
+    use_fixture(browser_firefox, context)
